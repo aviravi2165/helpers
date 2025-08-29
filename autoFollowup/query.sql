5e19e8fff9edc8ae990f2c5d739ee4bc8b6901f9
aviravi2165 <avi.ravi2165@gmail.com>
2025-08-29 15:55:11 +0530

    New changes

--- autoFollowup/query.sql
+++ autoFollowup/query.sql
@@ -1,3 +1,4 @@
+use ievo;
 SELECT 
 	tm.docCode,
 	um1.firstName + ' ' + um1.lastName AS 'Task To',
@@ -5,14 +6,20 @@ SELECT
 	um2.firstName + ' ' + um2.lastName AS 'Task By',
 	um2.phoneNumber AS 'Task By Contact',
 	tm.taskDetails,
-	tm.deadline
+	tm.taskToOn AS 'Task Created Date',
+	tm.deadline,
+	tm.acceptanceStatus,
+	tm.doneStatus,
+	tm.acknowledgeStatus
 FROM 
 	taskMgmt tm
 INNER JOIN usersMaster um1 ON tm.taskTo = um1.userId
 INNER JOIN deptMaster dm1 ON um1.deptId = dm1.id
-INNER JOIN usersMaster um2 ON tm.taskTo = um2.userId
+INNER JOIN usersMaster um2 ON tm.taskBy = um2.userId
 INNER JOIN deptMaster dm2 ON um2.deptId = dm2.id
 WHERE 
 	tm.acceptanceStatus <> 'REJECTED'
 	AND
 	tm.acknowledgeStatus = 0
+ORDER BY 
+	um1.firstName
